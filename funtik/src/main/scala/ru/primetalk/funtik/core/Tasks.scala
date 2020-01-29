a8e436ce1daff7f7fecaa8249512346fdb83a43a
Arseniy Zhizhelev <zhizhelev@primetalk.ru>
2020-01-29 23:31:40 +0300

    Add high-level task elementary model

---
+++ funtik/src/main/scala/ru/primetalk/funtik/core/Tasks.scala
@@ -0,0 +1,18 @@
+package ru.primetalk.funtik.core
+
+import ru.primetalk.funtik.environment.EnvironmentModel
+// Task should be able to represent the intent "bring me the red ball from my room".
+trait Tasks extends  EnvironmentModel {
+  sealed trait ObjectSpecification
+  case class LocationInfo(place: Place) extends ObjectSpecification
+  case class ObjectName(name: String) extends ObjectSpecification
+
+  sealed trait Task
+
+  /**
+   * Bring some object that satisfies the list of attributes.
+   */
+  case class BringAnObject(attributes: List[ObjectSpecification]) extends Task
+
+  val task: Task = BringAnObject(List(ObjectName("red ball"), LocationInfo(myRoom)))
+}
