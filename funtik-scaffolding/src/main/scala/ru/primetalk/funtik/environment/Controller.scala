aa8e5ca8a1714ad999dea4d153a6e6454a6a1c52
Sergey <skazancev@gmail.com>
2020-03-15 22:54:09 +0300

    room draw in scalaJs

--- funtik-scaffolding/src/main/scala/ru/primetalk/funtik/environment/Controller.scala
+++ funtik-scaffolding/src/main/scala/ru/primetalk/funtik/environment/Controller.scala
@@ -1,8 +1,11 @@
 package ru.primetalk.funtik.environment
 
 import org.scalajs.dom
+import ru.primetalk.funtik.environment.genereator.{BSPTree, Leaf, Tree, Node}
+import ru.primetalk.funtik.environment.genereator.utils.Random
+import ru.primetalk.funtik.environment.geom2d.Geom2dUtils._
 
-import scala.util.Random
+import scala.util.{Random => ScalaRandom}
 
 
 // Mutable part of the program
@@ -11,12 +14,22 @@ class Controller
   val ctx: dom.CanvasRenderingContext2D,
   val gameView: View[Display[Boolean]]
 ) {
-  private val rnd = new Random()
+  private val rnd = new ScalaRandom()
   private var currentGameState: Display[Boolean] = generateDisplay(rnd.nextInt())
 
   def generateDisplay(n: Int): Display[Boolean] = {
-    Display.showPoints(Seq((1,1), (79,59)), true, false)
+    val rect = Rectangle(0 -> 0, 79 -> 59)
+    val randoms = Random.stream(System.currentTimeMillis())
+    val roomTree = new BSPTree(10).generate(rect).runA(randoms).value
+    val points = toPoints(roomTree)
+    Display.showPoints(points, true, false)
   }
+
+  private def toPoints(tree: Tree[Rectangle]): List[Position] = tree match {
+    case Node(l, r) => toPoints(l) ++ toPoints(r)
+    case Leaf(rect) => rect.edges
+  }
+
 
   def redraw(): Unit = {
     gameView.render(currentGameState, ctx)
