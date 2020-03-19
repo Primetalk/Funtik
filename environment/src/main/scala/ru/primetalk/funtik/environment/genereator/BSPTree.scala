fea1a8c0477ab285730dc8217b87081ece7b9fdf
Arseniy Zhizhelev <zhizhelev@primetalk.ru>
2020-03-19 13:59:18 +0300

    Reduce to a single `apply`

--- environment/src/main/scala/ru/primetalk/funtik/environment/genereator/BSPTree.scala
+++ environment/src/main/scala/ru/primetalk/funtik/environment/genereator/BSPTree.scala
@@ -13,37 +13,27 @@ import ru.primetalk.funtik.environment.genereator.utils.Random.RandomState
  */
 object BSPTree {
   def apply(
-    initialRect: Rectangle,
     minSideSize: Int = 4,
     leafProbability: Double = 0.5
-  ): RandomState[Tree[Rectangle]] = {
-    generate(initialRect)(BSPTreeParams(minSideSize, leafProbability = leafProbability))
-  }
-
-  private case class BSPTreeParams(minSideSize: Int = 4, leafProbability: Double = 0.1) {
-    val minSplittableSize: Int = minSideSize * 2
-  }
-
-  private def generate(
+  )(
     boundRect: Rectangle
-  )(implicit ctx: BSPTreeParams = BSPTreeParams()
   ): RandomState[Tree[Rectangle]] = {
     val longestAxis = boundRect.getLongestAxis
     val splitAxis = OptionT
       .fromOption[RandomState](longestAxis)
       .getOrElseF(EnvironmentRandom.nextAxis)
     splitAxis.flatMap { axis =>
-      val shouldSplit = Random.nextProb(1.0 - ctx.leafProbability)
+      val shouldSplit = Random.nextProb(1.0 - leafProbability)
 
       def calcSplitPositionOpt(unit: Unit): RandomState[Option[Int]] = {
         val sideSize = boundRect.sideSize(axis)
-        Random.betweenOpt(ctx.minSideSize, sideSize - ctx.minSideSize)
+        Random.betweenOpt(minSideSize, sideSize - minSideSize)
       }
 
       def split(splitPosition: Int): RandomState[Tree[Rectangle]] = {
         val splitAxis = transpose(axis)
         val rects     = boundRect.splitL(splitAxis, splitPosition)
-        val rectTrees = rects.map(generate)
+        val rectTrees = rects.map(apply(minSideSize, leafProbability))
         val rRects = rectTrees.foldM(List[Tree[Rectangle]]()) {
           case (lst, rTree) =>
             rTree.map(tree => tree :: lst)
