f7825c01b86c5c2c74b5e103182d95ec662c5d85
Arseniy Zhizhelev <zhizhelev@primetalk.ru>
2020-04-19 20:28:49 +0300

    Add robot strategy

--- funtik-scaffolding/src/main/scala/ru/primetalk/funtik/environment/Funtik.scala
+++ funtik-scaffolding/src/main/scala/ru/primetalk/funtik/environment/Funtik.scala
@@ -2,13 +2,17 @@ package ru.primetalk.funtik.environment
 
 import org.scalajs.dom
 import org.scalajs.dom.raw.HTMLCanvasElement
+import ru.primetalk.funtik.core.RobotLoopT
 
 import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
 
 @JSExportTopLevel("Funtik")
 object Funtik {
+
+  object Game extends ViewWorldStateT with RobotLoopT with MechanicsImplT with ControllerT
+
   def main(args: Array[String]): Unit = {
-    println(ViewAll.currentRoom.name)
+    println(Game.currentRoom.name)
   }
 
   @JSExport("startFuntik")
@@ -16,7 +20,7 @@ object Funtik {
     val canvasElement = dom.document.getElementById(canvasId)
     val canvas = canvasElement.asInstanceOf[HTMLCanvasElement]
     val ctx = canvas.getContext("2d").asInstanceOf[dom.CanvasRenderingContext2D]
-    val controller = new Controller(ctx, MechanicsImpl, System.currentTimeMillis())
+    val controller = new Game.Controller(ctx, new Game.MechanicsImpl(Game.handleSensorData, Game.initialInternalRobotState), System.currentTimeMillis())
     controller.start()
   }
 
